fde056496c53a65a5210ec8c16d70a34e8573e5f
FANG DAI <65178777+fangdai-dear@users.noreply.github.com>
2024-07-18 14:27:30 +0800

    Update ResNet_main.sh

--- ResNet_main.sh
+++ ResNet_main.sh
@@ -1,298 +1,10 @@
-# export MODELNAME="Thyroid_PTC_1000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_1000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=296 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_PTC_1000+SD1000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_1000+SD1000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_PTC_1000+SD2000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_1000+SD2000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_PTC_2000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_2000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_PTC_2000+SD1000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_2000+SD1000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_PTC_2000+SD2000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_2000+SD2000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_FTC"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="FTC_all_train"
-# export VALID="FTC_all_valid"
-# export TEST="FTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-
-# export MODELNAME="Thyroid_FTC+SD1000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="FTC_all+SD1000_train"
-# export VALID="FTC_all_valid"
-# export TEST="FTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_FTC+SD2000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="FTC_all+SD2000_train"
-# export VALID="FTC_all_valid"
-# export TEST="FTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_PTC+SD4000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_all+SD4000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.0001  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_PTC+SD8000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_all+SD8000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.00005  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_MTC"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="MTC_train"
-# export VALID="MTC_test"
-# export TEST="MTC_test" 
-# export BATCH=298 
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.00005  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-# export MODELNAME="Thyroid_PTC_all+SD8000"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="PTC_all+SD8000_train"
-# export VALID="PTC_all_valid"
-# export TEST="PTC_all_test" 
-# export BATCH=200
-
-# CUDA_VISIBLE_DEVICES=1 python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.00005  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-
-# export MODELNAME="Thyroid_MTC"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="MTC_train"
-# export VALID="MTC_valid"
-# export TEST="MTC_test" 
-# export BATCH=200
-
-# python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.00005  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-
-# export MODELNAME="Thyroid_ATC"
-# export ARCH="resnet"
-# export IMAGEPATH="./dataset"
-# export TRAIN="ATC_train"
-# export VALID="ATC_valid"
-# export TEST="ATC_test" 
-# export BATCH=200
-
-# python main_train.py \
-#         --modelname $MODELNAME \
-#         --architecture $ARCH   \
-#         --imagepath $IMAGEPATH   \
-#         --train_data $TRAIN   \
-#         --valid_data $VALID   \
-#         --test_data $TEST  \
-#         --learning_rat 0.00005  \
-#         --batch_size $BATCH   \
-#         --num_epochs 120
-
-export MODELNAME="Thyroid_PTC_ATC"
+export MODELNAME="Thyroid_Benign&PTC"
 export ARCH="resnet"
 export IMAGEPATH="./dataset"
-export TRAIN="PTC_ATC_train"
-export VALID="PTC_ATC_valid"
-export TEST="PTC_ATC_test" 
-export BATCH=200
+export TRAIN="Benign&PTC_train"
+export VALID="Benign&PTC_valid"
+export TEST="Benign&PTC_test" 
+export BATCH=64
 
 python main_train.py \
         --modelname $MODELNAME \
@@ -303,5 +15,47 @@ python main_train.py \
         --test_data $TEST  \
         --learning_rat 0.0005  \
         --batch_size $BATCH   \
-        --num_epochs 120 \
-        --Class 5
+        --num_epochs 200 \
+        --Class 2
+
+
+
+export MODELNAME="Thyroid_Benign&FTC"
+export ARCH="resnet"
+export IMAGEPATH="./dataset"
+export TRAIN="Benign&FTC_train"
+export VALID="Benign&FTC_valid"
+export TEST="Benign&FTC_test" 
+export BATCH=64
+
+python main_train.py \
+        --modelname $MODELNAME \
+        --architecture $ARCH   \
+        --imagepath $IMAGEPATH   \
+        --train_data $TRAIN   \
+        --valid_data $VALID   \
+        --test_data $TEST  \
+        --learning_rat 0.0005  \
+        --batch_size $BATCH   \
+        --num_epochs 200 \
+        --Class 2
+
+export MODELNAME="Thyroid_Benign&MTC"
+export ARCH="resnet"
+export IMAGEPATH="./dataset"
+export TRAIN="Benign&MTC_train"
+export VALID="Benign&MTC_valid"
+export TEST="Benign&MTC_test" 
+export BATCH=64
+
+python main_train.py \
+        --modelname $MODELNAME \
+        --architecture $ARCH   \
+        --imagepath $IMAGEPATH   \
+        --train_data $TRAIN   \
+        --valid_data $VALID   \
+        --test_data $TEST  \
+        --learning_rat 0.0005  \
+        --batch_size $BATCH   \
+        --num_epochs 200 \
+        --Class 2
