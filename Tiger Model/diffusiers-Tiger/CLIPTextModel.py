735a774cbbb39f5eddd1d82b38b73722f94fcbc4
FANG DAI <65178777+fangdai-dear@users.noreply.github.com>
2024-09-21 11:19:42 +0800

    Update CLIPTextModel.py

--- Tiger Model/diffusiers-Tiger/CLIPTextModel.py
+++ Tiger Model/diffusiers-Tiger/CLIPTextModel.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2021 The OpenAI Team Authors and The HuggingFace Team. All rights reserved.
+# Copyright 2024 Hui Lu, Fang Dai, Siqiong Yao.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 """ PyTorch CLIP model."""
 
 
