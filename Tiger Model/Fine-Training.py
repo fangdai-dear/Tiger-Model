9766a2fa46abd346f57f4142ac2b8a286bdf5b29
FANG DAI <65178777+fangdai-dear@users.noreply.github.com>
2025-04-04 19:07:27 +0800

    Update Fine-Training.py

--- Tiger Model/Fine-Training.py
+++ Tiger Model/Fine-Training.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
 # coding=utf-8
-# Copyright 2024 Hui Lu, Fang Dai, Siqiong Yao.
+# Copyright 2024 Hui Lu, Fang Dai, Siqiong Yao
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,9 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# This file is modified based on the original implementation from the HuggingFace Inc. team (2023) https://github.com/huggingface/diffusers/.
-# Original code licensed under the Apache License, Version 2.0.
-# See: http://www.apache.org/licenses/LICENSE-2.0
+# This file includes modifications based on the original implementation from:
+# HuggingFace Inc. (2023), Diffusers Library, available at:
+# https://github.com/huggingface/diffusers
+# The original code is licensed under the Apache License, Version 2.0.
+
+
 import argparse
 import logging
 import math
